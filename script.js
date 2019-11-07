cf20bcff0b2f314df693ef4a981782700a6ac95f
tverdone <tverdone10@gmail.com>
2019-11-06 21:13:38 -0800

    Weather app working. Going to work on formatting and other features.

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// console.log($("#submit"))
+$("#submit").on("click", function(){    
+    var city = $(".user-input").val()
+// alert("asldjfasdfaklsdhjf")
+    $("div").removeClass("d-none")
+    console.log(city)
+
+
+
+
+$.ajax({
+    type: "POST",
+    url: `http://api.openweathermap.org/data/2.5/weather?q=${city},us&units=imperial&appid=89e5e9bfd9e3ee1833825011cf0ee408`,
+    dataType: "json",
+    success: function (data) {
+    console.log(data)
+    console.log(data.main.humidity)
+
+    
+    
+    $(".card-title").text(data.name)
+    $(".card-text").text(data.main.temp)
+    
+    
+
+    }
+})
+})
+    
+
