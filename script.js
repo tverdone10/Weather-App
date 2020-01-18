2fea744996af88a78ff58c167f81d8e587d2e105
tverdone <tverdone10@gmail.com>
2020-01-18 09:22:04 -0800

    ugly but works

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 // console.log($("#submit"))
 $("#submit").on("click", function(){    
     var city = $(".user-input").val()
-// alert("asldjfasdfaklsdhjf")
     $("div").removeClass("d-none")
     console.log(city)
 
@@ -14,6 +13,7 @@ $.ajax({
     url: `http://api.openweathermap.org/data/2.5/weather?q=${city},us&units=imperial&appid=89e5e9bfd9e3ee1833825011cf0ee408`,
     dataType: "json",
     success: function (data) {
+
     console.log(data)
     console.log(data.main.humidity)
 
@@ -25,7 +25,23 @@ $.ajax({
     
 
     }
-})
-})
+
     
+
+})
+
+$.ajax({
+    type: "POST",
+    url: `http://api.openweathermap.org/data/2.5/forecast?q=${city},us&units=imperial&appid=89e5e9bfd9e3ee1833825011cf0ee408`,
+    dataType: "json",
+    success: function (data1) {
+
+        console.log(data1)
+    
+
+    }
+
+
+})
+})
 
